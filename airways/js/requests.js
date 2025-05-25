92a81740b358e379a2306ea9b210d1b1ff912b6c
alex853 <alexxxey.kornev@gmail.com>
2025-05-25 21:32:22 +0300

    airways - some starting pages

---
+++ airways/js/requests.js
@@ -0,0 +1,98 @@
+
+const airwaysServiceUrl = 'https://d1.simforge.co.uk:7776';
+
+function loadCities(callback) {
+    $.ajax({
+        url: airwaysServiceUrl + '/geo/cities',
+        method: 'GET',
+        dataType: 'json',
+        success: function (response) {
+            if (callback) {
+                callback(response);
+            }
+        },
+        error: function (e) {
+            console.error("error loading city data");
+        }
+    });
+}
+
+function loadAirports(callback) {
+    $.ajax({
+        url: airwaysServiceUrl + '/geo/airports',
+        method: 'GET',
+        dataType: 'json',
+        success: function (response) {
+            if (callback) {
+                callback(response);
+            }
+        },
+        error: function (e) {
+            console.error("error loading airports data");
+        }
+    });
+}
+
+function loadAllAircraft(callback) {
+    $.ajax({
+        url: airwaysServiceUrl + '/aircraft/all',
+        method: 'GET',
+        dataType: 'json',
+        success: function (response) {
+            if (callback) {
+                callback(response);
+            }
+        },
+        error: function (e) {
+            console.error("error loading aircraft data");
+        }
+    });
+}
+
+function loadAllEventsToProcess(callback) {
+    $.ajax({
+        url: airwaysServiceUrl + '/events-to-process/all',
+        method: 'GET',
+        dataType: 'json',
+        success: function (response) {
+            if (callback) {
+                callback(response);
+            }
+        },
+        error: function (e) {
+            console.error("error loading events data");
+        }
+    });
+}
+
+function loadAllFlightMissions(callback) {
+    $.ajax({
+        url: airwaysServiceUrl + '/flight-mission/all',
+        method: 'GET',
+        dataType: 'json',
+        success: function (response) {
+            if (callback) {
+                callback(response);
+            }
+        },
+        error: function (e) {
+            console.error("error loading flight mission data");
+        }
+    });
+}
+
+function loadAllEventLogs(callback) {
+    $.ajax({
+        url: airwaysServiceUrl + '/event-log/all',
+        method: 'GET',
+        dataType: 'json',
+        success: function (response) {
+            if (callback) {
+                callback(response);
+            }
+        },
+        error: function (e) {
+            console.error("error loading event log");
+        }
+    });
+}
